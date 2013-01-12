8c78d2ab9048a88fd8b3add3e7bc81c223f5c687
a1219sg <a1219sg@aiit.ac.jp>
2013-01-12 16:56:46 +0900

    generated myadd.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myadd::Application.config.secret_token = '6d6bc304b247811ada933a6e5c1960027cb0663ed26c0ac220c727d8aa2287abd18a9aa09f3b6a77489186bb2a1e660319710c27d9ea51f19c9107ac8af542ba'
